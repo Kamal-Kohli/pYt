00a8afbc2377a517e1bdea25e5c69879b29ef90b
Kamal Kohli <kamalkohli@Kamals-MacBook-Pro.local>
2024-02-19 20:19:50 +0100

    Api

--- newsapi/views.py
+++ newsapi/views.py
@@ -4,11 +4,19 @@ API_KEY = '2228671f48644d399e8cc37fb14015f6'
 
 # Create your views here.
 def home(request):
-    url = f'https://newsapi.org/v2/top-headlines?country=us&apiKey={API_KEY}'
-    response = requests.get(url)
-    data = response.json()
-    
-    articles = data['articles']
+    country = request.GET.get('country')
+    category = request.GET.get('category')
+
+    if country:
+        url = f'https://newsapi.org/v2/top-headlines?country={country}&apiKey={API_KEY}'
+        response = requests.get(url)
+        data = response.json()
+        articles = data['articles']
+    else:
+        url = f'https://newsapi.org/v2/top-headlines?category={category}&apiKey={API_KEY}'
+        response = requests.get(url)
+        data = response.json()
+        articles = data['articles']
 
     context = {
         'articles' : articles
